d55e31d2c5952b7e0f3d6b621f726546fff0ee2e
Leonel Vega Vargas <leonelvegavargas@hotmail.com>
2017-03-24 23:46:05 +0000

    todoapp

---
+++ app/build/generated/source/buildConfig/debug/josedlujan/compras/de/lista/app/com/applistadecompras/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package josedlujan.compras.de.lista.app.com.applistadecompras;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "josedlujan.compras.de.lista.app.com.applistadecompras";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
